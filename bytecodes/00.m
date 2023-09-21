4c9816caaf4f0ea3c9f45394d1db74de2dcbbe74
serenalilian <serenalilian99@gmail.com>
2023-09-21 09:06:17 -0700

    monty

--- bytecodes/00.m
+++ bytecodes/00.m
@@ -1,16 +1,7 @@
-push 0
+             
+
 push 1
 push 2
-      push 3
-                  pall           
-  push 4
-    push 5   
-      push    6      
-pall
+push 3
 
-
-
-
-
-push 7
-pall
+                 pall
